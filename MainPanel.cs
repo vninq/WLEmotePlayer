96e122c9fb47256cf72381ef4b60601ad31ababa
vninq <abeekud26@gmail.com>
2026-07-15 19:42:29 -0400

    Add files via upload

---
+++ MainPanel.cs
@@ -0,0 +1,108 @@
+﻿using ShadowLib;
+using System;
+using UnityEngine;
+using UniverseLib.UI;
+
+namespace WLEmotePlayer
+{
+    public class MainPanel : UniverseLib.UI.Panels.PanelBase
+    {
+        // emots
+        public string emoteHappy = "Happy";
+        public string emoteSad = "Crying";
+        public string emoteAngry = "Angry";
+        public string emoteHeart = "Heart";
+        public MainPanel(UIBase owner) : base(owner) { }
+        public override string Name => "Emote Player";
+        public override int MinWidth => 500;
+        public override int MinHeight => 600;
+        public override Vector2 DefaultAnchorMin => new Vector2(0.5f, 0.5f);
+        public override Vector2 DefaultAnchorMax => new Vector2(0.5f, 0.5f);
+        public override Vector2 DefaultPosition => new Vector2((float)(-(float)MinWidth / 2), (MinHeight / 2));
+        public override bool CanDragAndResize => true;
+
+        public static PlayerCharacter GetMyCharacter()
+        {
+            if (UnitySingleton<GameInstance>.Instance.GetGamemode() == null)
+            {
+                return null;
+            }
+            else
+            {
+                foreach (PlayerCharacter playerCharacter in UnitySingleton<GameInstance>.Instance.GetPlayerCharacters())
+                {
+                    if (playerCharacter.networkObject.IsOwner())
+                    {
+                        return playerCharacter;
+                    }
+                }
+                return null;
+            }
+        }
+
+        // ui stuff
+        protected override void ConstructPanelContent()
+        {
+            UIHelper ui = new UIHelper(ContentRoot);
+
+            ui.AddSpacer(5);
+            ui.CreateLabel("Playable Emotes");
+            ui.AddSpacer(6);
+            ui.CreateButton("Happy", () => PlayEmoteHappy(), "playEmoteB");
+            ui.AddSpacer(3);
+            ui.CreateButton("Angry", () => PlayEmoteAngry(), "playEmoteB");
+            ui.AddSpacer(3);
+            ui.CreateButton("Sad", () => PlayEmoteSad(), "playEmoteB");
+            ui.AddSpacer(3);
+            ui.CreateButton("Heart", () => PlayEmoteHeart(), "playEmoteB");
+        }
+
+        public void PlayEmoteAngry()
+        {
+            PlayerCharacter playerCharacter = GetMyCharacter();
+            if (playerCharacter != null)
+            {
+                playerCharacter.PlayEmote(emoteAngry);
+            }
+
+        }
+
+        public void PlayEmoteHappy()
+        {
+            PlayerCharacter playerCharacter = GetMyCharacter();
+            if (playerCharacter != null)
+            {
+                playerCharacter.PlayEmote(emoteHappy);
+            }
+        }
+
+        public void PlayEmoteSad()
+        {
+            PlayerCharacter playerCharacter = GetMyCharacter();
+            if (playerCharacter != null)
+            {
+                playerCharacter.PlayEmote(emoteSad);
+            }
+        }
+
+        public void PlayEmoteHeart()
+        {
+            PlayerCharacter playerCharacter = GetMyCharacter();
+            if (playerCharacter != null)
+            {
+                playerCharacter.PlayEmote(emoteHeart);
+            }
+        }
+
+        public void _SetActive(bool b)
+        {
+            SetActive(b);
+        }
+
+        protected override void OnClosePanelClicked()
+        {
+            Plugin.SetUIEnabled(false);
+        }
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,62 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using UniverseLib.Config;
+using UniverseLib.UI;
+
+namespace WLEmotePlayer
+{
+    [BepInPlugin("EmotePlayer", "emoteplayer", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static UIBase UIBase { get; private set; }
+        public static MainPanel MainPanel { get; private set; }
+        public static Plugin Instance { get; private set; }
+
+        private void Awake()
+        {
+            Instance = this;
+
+            UniverseLibConfig config = new UniverseLibConfig()
+            {
+                Force_Unlock_Mouse = true
+            };
+
+            UniverseLib.Universe.Init(1f, OnUIInitialized, (x, y) => { }, config);
+
+            Logger.LogInfo("Plugin EmotePlayer is loaded!");
+        }
+
+
+        private static void OnUIInitialized()
+        {
+            UIBase = UniversalUI.RegisterUI("vninq.emoteplayer", UIUpdate);
+
+            MainPanel = new MainPanel(UIBase);
+
+            UIBase.Enabled = false;
+        }
+
+        public static void UIUpdate()
+        {
+
+        }
+
+        public static void SetUIEnabled(bool enabled)
+        {
+            if (UIBase == null) throw new ArgumentNullException(nameof(UIBase));
+            UIBase.Enabled = enabled;
+            if (MainPanel == null) throw new ArgumentNullException(nameof(UIBase));
+            MainPanel._SetActive(enabled);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F4))
+            {
+                SetUIEnabled(!UIBase.Enabled);
+            }
+        }
+
+    }
+}
