37c4ff95bddc0eb76d1a1ebd75645b4295cbbddd
vninq <abeekud26@gmail.com>
2026-08-28 13:21:30 -0400

    Delete PluginInfo.cs

--- PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmotePlayer
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "EmotePlayer";
-        public const string PLUGIN_NAME = "EmotePlater";
-        public const string PLUGIN_VRESION = "0.0.1";
-    }
-}
