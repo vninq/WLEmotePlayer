5db794d5da04772ed2b73ee74279697635fac3c3
vninq <abeekud26@gmail.com>
2026-08-28 13:20:52 -0400

    better now

---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace EmotePlayer
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "EmotePlayer";
+        public const string PLUGIN_NAME = "EmotePlater";
+        public const string PLUGIN_VRESION = "0.0.1";
+    }
+}
