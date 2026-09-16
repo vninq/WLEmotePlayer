6e6845e48066f81e0da7709798f2e3bc6a9b98c8
vninq <abeekud26@gmail.com>
2026-09-16 14:02:35 -0400

    Update Plugin.cs
    Removed dependency attributes for lstwoMODS.

--- Plugin.cs
+++ Plugin.cs
@@ -1,22 +1,19 @@
-﻿using BepInEx;
+using BepInEx;
 using BepInEx.Logging;
-using System.Reflection;
 
 namespace EmotePlayer
 {
     [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-    [BepInDependency("net.lstwo.lstwomods_core")]
-    [BepInDependency("lstwoMODS_WobblyLife")]
+    //[BepInDependency("net.lstwo.lstwomods_core")]
+    //[BepInDependency("lstwoMODS_WobblyLife")]
     public class Plugin : BaseUnityPlugin
     {
         internal static new ManualLogSource Logger;
         private void Awake()
         {
             Logger = base.Logger;
-            // Plugin startup logic
+
             Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
-
-
 }
