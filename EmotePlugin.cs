5f76ad8d0aa6e41047fcafa51359e4dec45093c4
sunk24 <abeekud26@gmail.com>
2026-09-19 16:57:25 -0400

    why it works?
    this is not a string

--- EmotePlugin.cs
+++ EmotePlugin.cs
@@ -48,7 +48,7 @@ namespace EmotePlayer
             Wave,
             Dance,
             Thumbs_Up,
-            Ha Ha,
+            Ha_Ha,
             Thumbs_Down,
             Grumpy
         }
